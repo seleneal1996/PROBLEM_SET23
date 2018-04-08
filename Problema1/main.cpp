9093e1e622d481422cd7f035ccf18957d6ff98b9
seleneal1996 <selenemsb@hotmail.com>
2018-04-08 13:25:22 -0500

    problema1

--- Problema1/main.cpp
+++ Problema1/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+void printNTimes(char *msg,int n)
+{
+    for(int i=1;i<n;i++)
+    {
+     cout<<msg<<"\n";
+    }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int n;
+    char *msg=new char[100];
+    cout<<"Ingrese mensaje:"<<endl;
+    cin>>msg;
+    cout<<"Ingrese n:"<<endl;
+    cin>>n;
+    printNTimes(msg,n);
+    cout<<msg<<endl;
+
 }
